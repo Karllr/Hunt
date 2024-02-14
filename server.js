785f4212739d41593727dd3a98d9da5e853e90c9
Jadie <117045702+Karllr@users.noreply.github.com>
2024-02-14 18:04:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ addStone();
 addOres();
 checkOverlap();
 var app=express();
-var server=app.listen('216.24.57.253');
+var server=app.listen('0.0.0.0');
 app.use(express.static('public'))
 console.log("This server happens to be running");
 
